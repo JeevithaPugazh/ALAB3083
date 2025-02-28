02254a9cdb46239919225634befb7538ab94bf46
Jeevitha <jeevitha.pugazh@gmail.com>
2025-02-28 18:14:51 -0500

    Updated for loop in part 3

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ console.log("---- PART 3 ----");
 const inputStr =
   "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
 const strRows = inputStr.split("\n");
-for (let i = 0; i < strRows.length - 1; i++) {
+for (let i = 0; i <= strRows.length - 1; i++) {
 
   const cell = strRows[i].split(",");
   console.log(cell[0], cell[1], cell[2], cell[3]);
